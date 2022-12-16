30819d5ba61572cf59149303656e8e174d2e75dd
rovxxmjxx <miju3545@gmail.com>
2022-12-16 14:17:56 +0900

    feat: redux setting

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { useRoutes } from 'react-router';
 import { routes } from './routes';
-import { getClient } from './queryClient';
+import { getClient } from './utils/queryClient';
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { Provider } from 'react-redux';
+import { getStore } from './redux/index';
 
 function App() {
   const element = useRoutes(routes);
   const queryClient = getClient();
+  const store = getStore();
 
   return (
     <QueryClientProvider client={queryClient}>
-      {element}
+      <Provider store={store}>{element}</Provider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
