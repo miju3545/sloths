c011efdb01b8c3d473635295e4b0c88a13080abe
rovxxmjxx <miju3545@gmail.com>
2022-12-16 20:51:19 +0900

    chore

---
+++ www/src/_layouts.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <div>
+      <Suspense fallback={'loading...'}>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
+};
+
+export default Layout;
